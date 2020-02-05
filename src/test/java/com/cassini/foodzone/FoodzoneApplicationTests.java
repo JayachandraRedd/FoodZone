4ab7701e7b59cf854d1e7994ed689ee22309347b
priyas35 email = priyaharisubi@gmail.com <User1@LP-5CD9176LCF.HCLT.CORP.HCL.IN>
2020-02-05 13:12:49 +0530

    added src and pom

---
+++ src/test/java/com/cassini/foodzone/FoodzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cassini.foodzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
